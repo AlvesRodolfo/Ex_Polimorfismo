d8cdcf32703a93abe0159b5c2c8e17b4f40a6c4a
Rodolfo Alves <rodolfoalveseng@gmail.com>
2019-01-17 23:32:40 -0200

    Exercicio de Fixacao

---
+++ src/entities/UsedProduct.java
@@ -0,0 +1,39 @@
+package entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class UsedProduct extends Product {
+	
+	
+	private static final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy"); //deve-se instanciar na classe também
+	
+	private Date manufactureDate;
+	
+	public UsedProduct () {
+		
+	}
+
+	public UsedProduct(String name, Double price, Date manufactureDate) {
+		super(name, price);
+		this.manufactureDate = manufactureDate;
+	}
+
+	public Date getManufactureDate() {
+		return manufactureDate;
+	}
+
+	public void setManufactureDate(Date manufactureDate) {
+		this.manufactureDate = manufactureDate;
+	}
+	
+	@Override
+	public String priceTag() {
+		return getName() 
+				+ " (used) $ " 
+				+ String.format("%.2f", getPrice())
+				+ " (Manufacture date: "
+				+ sdf.format(manufactureDate)  //define-se o formato da data
+				+ ")";
+}
+}
